4a4a87543dcd4c08ceb8bb318eae3d638cbaa59f
Blockchain-Key <1416893908@qq.com>
2022-11-29 16:50:33 +0800

    修改title和links

--- frontend/.umirc.ts
+++ frontend/.umirc.ts
@@ -2,4 +2,6 @@ export default {
   npmClient: 'yarn',
   tailwindcss: {},
   plugins: ['@umijs/plugins/dist/tailwindcss'],
+  title: 'Move SBT',
+  links: [{ rel: 'icon', href: '/sui.png', type: 'image/x-icon' }],
 }
